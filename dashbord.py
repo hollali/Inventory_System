18bedde525f7cfb93fda19e3a19fd20c34efa350
hollali <dheztinykartel@gmail.com>
2024-08-17 14:10:25 +0000

    "Updated background image and title label placement in dashboard.py"

--- dashbord.py
+++ dashbord.py
@@ -7,8 +7,8 @@ window.geometry('1270x700+0+0')
 window.resizable(0,0)
 window.config(bg='white')
 
-bg_image=PhotoImage(file='inventory.png')
+bg_image=PhotoImage(file='inventory1.png')
 titleLabel=Label(window,image=bg_image,compound=LEFT,text="Inventory Managment System",font=('times new roman',40,'bold'),bg='#010c48',fg='white')
-titleLabel.place(x=0,y=0)
+titleLabel.place(x=0,y=0,relwidth=1)
 
 window.mainloop()
