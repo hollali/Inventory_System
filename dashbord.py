0e1d1f7687e417d24f5898de72feda7d6ead21fa
hollali <dheztinykartel@gmail.com>
2024-08-17 12:23:38 +0000

    first commit

---
+++ dashbord.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+window=Tk()
+window.title("Dashboard")
+window.geometry('1270x700+0+0')
+window.resizable(0,0)
+window.config(bg='white')
+
+titleLabel=Label(window)
+
+window.mainloop()
