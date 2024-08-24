90e848a7596992fcf42d2256d28c9e3f8aad5e01
hollali <dheztinykartel@gmail.com>
2024-08-24 19:34:09 +0000

    "Updated employee.py: changed database table column name from 'id' to 'empid', added add_employee function, and modified employee_form function to match the column name change."

--- employee.py
+++ employee.py
@@ -10,12 +10,21 @@ def connect_database():
         cursor = connection.cursor()
     except:
         messagebox.showerror('Error','Connection Failed')
-        return
+        return None,None
     cursor.execute('CREATE DATABASE IF NOT EXISTS inventory_system')
     cursor.execute('USE inventory_system')
-    cursor.execute('CREATE TABLE IF NOT EXISTS employee_data (id INT PRIMARY KEY, name VARCHAR(100),gender VARCHAR(50),email VARCHAR(100),number VARCHAR(15),Dob VARCHAR(30),salary VARCHAR(50),address VARCHAR(50),usertype VARCHAR(50),password VARCHAR(50))')
+    cursor.execute('CREATE TABLE IF NOT EXISTS employee_data (empid INT PRIMARY KEY, name VARCHAR(100),gender VARCHAR(50),email VARCHAR(100),number VARCHAR(15),dob VARCHAR(30),salary VARCHAR(50),address VARCHAR(50),usertype VARCHAR(50),password VARCHAR(50))')
+    return cursor,connection
 
-connect_database()    
+
+def add_employee(empid,name,gender,email,number,dob,salary,address,usertype,password):
+    if (empid=='' or name=='' or email=='' or number=='' or gender=='Select Gender' or  salary==''or address=='\n' or usertype=='Employee Type' or password==''):
+        messagebox.showerror('Error','All fields are required')
+    else:
+        cursor,connection=connect_database()
+        if not cursor or connection:
+            return
+        cursor.execute()     
 
 #!Function Port
 def employee_form(window):
@@ -52,31 +61,31 @@ def employee_form(window):
     horizontal_scrollbar=Scrollbar(top_frame,orient=HORIZONTAL)
     vertical_scrollbar=Scrollbar(top_frame,orient=VERTICAL)
 
-    employee_treeview=ttk.Treeview(top_frame,columns=('id','name','gender','email','number','Dob','salary','address','usertype','password'),show='headings',yscrollcommand=vertical_scrollbar.set,xscrollcommand=horizontal_scrollbar.set)
+    employee_treeview=ttk.Treeview(top_frame,columns=('empid','name','gender','email','number','dob','salary','address','usertype','password'),show='headings',yscrollcommand=vertical_scrollbar.set,xscrollcommand=horizontal_scrollbar.set)
     horizontal_scrollbar.pack(side=BOTTOM,fill=X)
     vertical_scrollbar.pack(side=RIGHT,fill=Y,padx=(10,0))
     horizontal_scrollbar.config(command=employee_treeview.xview)
     vertical_scrollbar.config(command=employee_treeview.yview)
     employee_treeview.pack(pady=(10,0))
 
-    employee_treeview.heading('id',text='EmpId')
+    employee_treeview.heading('empid',text='Employee Id')
     employee_treeview.heading('name',text='Name')
     employee_treeview.heading('gender',text='Gender')
     employee_treeview.heading('email',text='Email')
     employee_treeview.heading('number',text='Phone Number')
-    employee_treeview.heading('Dob',text='Dob')
+    employee_treeview.heading('dob',text='Date of Birth')
     employee_treeview.heading('salary',text='Salary')
     employee_treeview.heading('address',text='Address')
     employee_treeview.heading('usertype',text='User Type')
     employee_treeview.heading('password',text='Password')
 
-    employee_treeview.column('id',width=120)
+    employee_treeview.column('empid',width=120)
     employee_treeview.column('name',width=240)
     employee_treeview.column('gender',width=140)
     employee_treeview.column('email',width=260) 
     employee_treeview.column('salary',width=160)
     employee_treeview.column('number',width=200)
-    employee_treeview.column('Dob',width=140)
+    employee_treeview.column('dob',width=140)
     employee_treeview.column('address',width=180)
     employee_treeview.column('usertype',width=140)
     employee_treeview.column('password',width=140)
@@ -84,7 +93,7 @@ def employee_form(window):
     detail_frame=Frame(employee_frame,bg='white')
     detail_frame.place(x=50,y=300)
 
-    empid_label=Label(detail_frame,text="Id",font=('times new roman',16,'bold'),bg='white')
+    empid_label=Label(detail_frame,text="Employee Id",font=('times new roman',16,'bold'),bg='white')
     empid_label.grid(row=0,column=0,padx=20,pady=10)
     empid_entry=Entry(detail_frame,font=('times new roman',16,'bold'),bg='lightyellow')
     empid_entry.grid(row=0,column=1,padx=20,pady=10)
@@ -139,7 +148,7 @@ def employee_form(window):
     button_frame=Frame(employee_frame,bg='white')
     button_frame.place(x=400,y=530)
 
-    add_button=Button(button_frame,text='Add',font=('times new roman',12),width=10,cursor='hand2',fg='white',bg='#0f4d7d')
+    add_button=Button(button_frame,text='Add',font=('times new roman',12),width=10,cursor='hand2',fg='white',bg='#0f4d7d',command=lambda:add_employee(empid_entry.get(),name_entry.get(),email_entry.get(),number_entry.get(),dob_date_entry.get(),gender_combobox,salary_entry.get(),address_text.get(1.0,END),usertype_combobox.get(),password_entry.get()))
     add_button.grid(row=0,column=0,padx=20)
 
     update_button=Button(button_frame,text='Update',font=('times new roman',12),width=10,cursor='hand2',fg='white',bg='#0f4d7d')
